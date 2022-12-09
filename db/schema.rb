c1cca32424dcf585e849dcd84baceff75b841427
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2022-12-09 23:58:55 +0000

    [follow]

--- db/schema.rb
+++ db/schema.rb
@@ -77,8 +77,8 @@ ActiveRecord::Schema.define(version: 2022_12_05_133237) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "following_id", null: false
     t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
