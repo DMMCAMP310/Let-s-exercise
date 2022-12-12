82783710124039a594a2b333c8572f3fcbd81c91
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2022-12-12 13:05:45 +0000

    レイアウト調整

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,6 +7,7 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @trainings = @user.trainings
   end
 
   def edit
---
+++ test/models/search_tag_bind_training_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SearchTagBindTrainingTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
