e21cd43a409b705c44da42675b19c16870e90882
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2022-12-13 09:54:11 +0000

    [レイアウト修正]

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,6 @@ Rails.application.routes.draw do
   
   namespace :admin do
     resources :users, only: [:index, :show, :edit, :update]
-    resources :trainings, only: [:index]
+    resources :trainings, only: [:index, :destroy]
   end
 end
