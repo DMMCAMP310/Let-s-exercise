f319f735d31bbcdcbaca93647209f92a0d4ad623
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2022-12-17 11:52:47 +0000

    chatsのmigrationファイルを移動

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_15_134527) do
+ActiveRecord::Schema.define(version: 2022_12_17_114803) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
