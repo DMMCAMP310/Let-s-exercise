f650b2b241f97e0912998d9dd8dc05729cc03692
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2022-12-24 16:32:12 +0000

    [調整]

--- app/controllers/public/week_enables_controller.rb
+++ app/controllers/public/week_enables_controller.rb
@@ -1,18 +1,18 @@
 class Public::WeekEnablesController < ApplicationController
   def update
     week_ids = params[:plan][:week_ids]
-    
+
     week_ids.shift
     plan = Plan.find(params[:plan_id])
     plan.week_enables.each do |week_enable|
-   
+
       if  week_ids.include?(week_enable.week.id.to_s)
         week_enable.update(checked: true)
       else
          week_enable.update(checked: false)
       end
     end
-    
+
      redirect_to working_plans_path
   end
 
@@ -21,7 +21,7 @@ class Public::WeekEnablesController < ApplicationController
       Week.all.each do |week|
         plan.week_enables.create(week_id: week.id)
       end
-    
+
     redirect_to working_plans_path
   end
 end
