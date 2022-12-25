1d5a448d6a2362de59b8bc842cfdc4bc6edfe1fc
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2022-12-25 07:43:27 +0000

    [routes修正]

--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,6 @@ Rails.application.routes.draw do
     end
     resources :merits, only: [:index, :create, :edit, :update]
     resources :causes, only: [:index, :create, :edit, :update]
-    resources :enables, only: [:index, :create, :update, :destroy]
   
     get 'search' => 'searches#search'
   end
