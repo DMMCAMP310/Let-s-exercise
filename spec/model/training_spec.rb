1e7a434cb8f543d1e000e2a343f6947978ea69e9
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2023-01-25 03:59:02 +0000

    [Rspec作成]

---
+++ spec/model/training_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe 'Trainingモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { training.valid? }
+    
+    let(:user) {create(:user) }
+    let!(:training) { build(:training, user_id: user.id) }
+    
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        training.name = ''
+        is_expected.to eq false
+      end
+    end
+    
+    context 'goalカラム' do
+      it '空欄でないこと' do
+        training.goal = ''
+        is_expected.to eq false
+      end
+    end
+    
+    context 'introductionカラム' do
+      it '空欄ではないこと' do
+        training.introduction = ''
+        is_expected.to eq false
+      end
+    end
+  end
+  
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Training.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+    context 'Likeモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Training.reflect_on_association(:likes).macro).to eq :has_many
+      end
+    end
+    context 'Training_commentモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Training.reflect_on_association(:training_comments).macro).to eq :has_many
+      end
+    end
+    context 'Notificationsモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Training.reflect_on_association(:notifications).macro).to eq :has_many
+      end
+    end
+  end
+end
---
+++ spec/model/user_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'Userモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { user.valid? }
+  end
+end
